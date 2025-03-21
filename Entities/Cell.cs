af9a0ed1159b6ea9406e5afa81ceec19208cdc71
Bruno Dopico <brunomlbw@gmail.com>
2025-03-21 22:42:15 +0100

    Moved entities to a separate folder and refactored the classes to simplify them

---
+++ Entities/Cell.cs
@@ -0,0 +1,32 @@
+﻿using Snake_Game.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game.Entities
+{
+    class Cell
+    {
+        public Cell(CellType type, int value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        //Type of the cell, possible values: 'V':Void, 'S':Snake, 'F':Fruit, 'O':Obstacle
+        public CellType Type { get; set; }
+
+        /**
+         * Value of the cell:
+         * For snake: Indicates the snake's next part direction where 0: left, 1: right, 2: up, 3: down
+         * For fruit: Indicates the fruit's added length to the snake
+         * For obstacle: 0: Wall, 1: Terrain
+        **/
+        public int Value { get; set; }
+
+    }
+}
+
+    
---
+++ Entities/Snake.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game.Entities
+{
+    class Snake
+    {
+        private int head_x;
+        private int head_y;
+        private int tail_x;
+        private int tail_y;
+        private int size;
+        private int growing;
+
+        //CONSTRUCTOR
+        public Snake(int initialPosition_x, int initialPosition_y, int initialSize)
+        {
+            head_x = initialPosition_x;
+            head_y = initialPosition_y;
+            tail_x = initialPosition_x;
+            tail_y = initialPosition_y;
+            size = initialSize;
+            growing = initialSize -1;
+        }
+
+        //SETTERS & GETTERS
+
+        public int Size { get => size; set => size = value; }
+        public int Growing { get => growing; set => growing = value; }
+        public int Head_x { get => head_x; set => head_x = value; }
+        public int Head_y { get => head_y; set => head_y = value; }
+        public int Tail_x { get => tail_x; set => tail_x = value; }
+        public int Tail_y { get => tail_y; set => tail_y = value; }
+
+        //METHODS
+        public void MoveHead(int direction)
+        {
+            
+            switch (direction)
+            {
+                case 0:
+                    head_x --;
+                    break;
+                case 1:
+                    head_x ++;
+                    break;
+                case 2:
+                    head_y --;
+                    break;
+                case 3:
+                    head_y ++;
+                    break;
+            }
+        }
+
+        public void MoveTail(int direction)
+        {
+
+            switch (direction)
+            {
+                case 0:
+                    tail_x--;
+                    break;
+                case 1:
+                    tail_x++;
+                    break;
+                case 2:
+                    tail_y--;
+                    break;
+                case 3:
+                    tail_y++;
+                    break;
+            }
+        }
+
+    }
+}
